040cc5e9f20040ff450b0af4b9cdf380a8ede42a
Johnathan Lavoie <johnathan.lavoie@ontariotechu.net>
2025-02-13 14:29:01 -0500

    added a function for getting all tasks on a specific day

--- flask_testing.py
+++ flask_testing.py
@@ -1,9 +1,12 @@
 import flask
 from flask import request
 from Task.Task import Task
+from Task.Task import Event
+from datetime import datetime
 
 # Create the application.
 APP = flask.Flask(__name__)
+tasks = []
 
 
 @APP.route('/')
@@ -20,11 +23,21 @@ def add_event():
     priority = request.args.get('priority')
     date = request.args.get('date')
 
-    e = Task(name, description, priority, date)
+    task = Task(name, description, priority, date)
 
-    print(e.id)
+    tasks.append(task)
 
-    return flask.jsonify({'event': e.id})
+    return flask.jsonify({'event': task.id})
+
+
+@APP.route('/get_daily_task', methods=['GET'])
+def get_daily_events():
+    date = [request.args.get('year'), request.args.get('month'), request.args.get('day')]
+    date = datetime(date[0], date[1], date[2])
+
+    tasks_on_date = [task for task in tasks if task.deadline == date]
+
+    return flask.jsonify({'tasks': [tasks_on_date]})
 
 
 if __name__ == '__main__':
